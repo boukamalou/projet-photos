8f2748ced931c710bf676d66ac4f297169ca9225
clayroth <clayration@gmail.com>
2020-04-01 17:56:49 +0200

    s

--- controller/connexion.php
+++ controller/connexion.php
@@ -1,15 +1,14 @@
 <?php
-/*
-MESSAGE :
-0 = ''
-1 = Utilisateur Créée
-2 = confirmation de mot de pass different du mot de pass
-3 = Utilisateur Inconnu
-4 = Formulaire incomplet
-5 = Erreur de connection a la BDD 
-6 = Mot de passe incorrect
-7 = Utilisateur inconnu
-*/
+
+/**Liste des message d erreur : 
+ * 1 = Utilisateur Créée
+ * 2 = confirmation de mot de pass different du mot de pass
+ * 3 = Utilisateur Inconnu
+ * 4 = Formulaire incomplet
+ * 5 = Erreur de connection a la BDD
+ * 6 = Mot de passe incorrect
+ * 7 = Utilisateur inconnu 
+ */
 session_start();
 
 $dsn = 'mysql:host=localhost;dbname=projet_photo;charset=utf8';
