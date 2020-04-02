b3b629ce0884a2b1b4584680136dc0a7c90f4b31
clayroth <clayration@gmail.com>
2020-04-02 15:45:50 +0200

    Photo en ligne

--- views/store.php
+++ views/store.php
@@ -23,7 +23,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Titre de la photo </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image2.png" alt="Card image cap">
                     <div class="row">
                         <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
                     </div>
@@ -31,7 +31,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Some more card content </div>
-                <img class="card-img-bottom" src="../src/image2.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image3.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
             </div>
          
@@ -42,7 +42,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Titre de la photo </div>
-                <img class="card-img-bottom" src="../src/image3.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image4.png" alt="Card image cap">
                     <div class="row">
                         <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
                     </div>
@@ -50,7 +50,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1 ">
                 <div class="card-body"> Some more card content </div>
-                <img class="card-img-bottom" src="../src/image4.jpg" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image5.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
             </div>
         </div>
@@ -59,7 +59,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Titre de la photo </div>
-                <img class="card-img-bottom" src="../src/image4.jpg" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image6.png" alt="Card image cap">
                     <div class="row">
                         <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
                     </div>
@@ -67,7 +67,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Some more card content </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image7.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
             </div>
         </div>
@@ -76,7 +76,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Titre de la photo </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image8.png" alt="Card image cap">
                     <div class="row">
                         <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
                     </div>
@@ -84,7 +84,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Some more card content </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image9.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
             </div>
         </div>
@@ -93,7 +93,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Titre de la photo </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image10.png" alt="Card image cap">
                     <div class="row">
                         <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
                     </div>
@@ -101,7 +101,7 @@
 
             <div class="card col-sm-12 col-md-5 m-1">
                 <div class="card-body"> Some more card content </div>
-                <img class="card-img-bottom" src="../src/image1.png" alt="Card image cap">
+                <img class="card-img-bottom" src="../src/image11.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
             </div>
         </div>
