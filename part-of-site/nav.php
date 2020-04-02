e0c43513431f8a7da532a22a9f52b4d0486175e5
clayroth <clayration@gmail.com>
2020-04-02 22:09:21 +0200

    w

--- part-of-site/nav.php
+++ part-of-site/nav.php
@@ -1,8 +1,8 @@
   
     <header class="container mb-5">
         <div class="d-flex flex-row justify-content-end">
-            <a class="btn btn-outline-dark m-1" href="">connexion</a>
-            <a class="btn btn-outline-dark m-1" href="">Inscription</a>
+            <a class="btn btn-outline-dark m-1" href="../views/login.php">connexion</a>
+            <a class="btn btn-outline-dark m-1" href="../views/register.php">Inscription</a>
             
         </div>
         <h1 class="ml-5" style="font-family: 'Roboto Mono', monospace">A WORLD OF FACES</h1>
@@ -10,8 +10,8 @@
     </header>
 
    <nav class="navbar d-flex flex-row justify-content-around mb-5 border rounded" style="border-color:#DCDCDC">
-        <a class="btn btn-outline-secondary" href="">ACCUEIL</a>
-        <a class="btn btn-outline-secondary" href="">QUI SUIS JE</a>
-        <a class="btn btn-outline-secondary" href="">STORE</a>
+        <a class="btn btn-outline-secondary" href="../index.php">ACCUEIL</a>
+        <a class="btn btn-outline-secondary" href="../views/presentation.php">QUI SUIS JE</a>
+        <a class="btn btn-outline-secondary" href="../views/store.php">STORE</a>
     </nav>
 
--- views/register.php
+++ views/register.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="container"> 
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -17,6 +17,35 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   
+
+    
+    <?php include "../part-of-site/nav.php" ?>
+    
+    <div id="carouselFadeExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="../src/imageC2" data-src="holder.js/900x400?theme=social" alt="First slide">
+        </div>
+        
+        <div class="carousel-item">
+        <img class="d-block w-100" src="../src/imageC1" data-src="holder.js/900x400?theme=industrial" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="../src/imageC3" data-src="holder.js/900x400?theme=industrial" alt="Second slide">
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselFadeExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselFadeExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+
+
     <div class="container d-flex flex-column justify-content-center align-items-center flex-wrap"> <!-- Container du formulaire -->
       <div class="p-4 text-center w-50">
           <?php // Gestion de l affichage des messages
