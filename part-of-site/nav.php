f0fdd2289ca043b532597f44a5f6e650e762dde2
clayroth <clayration@gmail.com>
2020-04-02 18:24:02 +0200

    maj

---
+++ part-of-site/nav.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <!-- GoogleFont -->
+     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto+Mono&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+    <header class="container mb-5">
+        <div class="d-flex flex-row justify-content-end">
+            <a class="btn btn-outline-dark m-1" href="">connexion</a>
+            <a class="btn btn-outline-dark m-1" href="">Inscription</a>
+            
+        </div>
+        <h1 class="ml-5" style="font-family: 'Roboto Mono', monospace">A WORLD OF FACES</h1>
+        <h3  style="font-family: 'Indie Flower',cursive;margin-left: 15em">plus qu'un voyage . . .</h3>
+    </header>
+
+   <nav class="navbar d-flex flex-row justify-content-around mb-5 border rounded" style="border-color:#DCDCDC">
+        <a class="btn btn-outline-secondary" href="">ACCUEIL</a>
+        <a class="btn btn-outline-secondary" href="">QUI SUIS JE</a>
+        <a class="btn btn-outline-secondary" href="">STORE</a>
+    </nav>
+
--- views/store.php
+++ views/store.php
@@ -1,44 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- GoogleFont -->
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto+Mono&display=swap" rel="stylesheet">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-    <body class="container">
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
-    <header class="container mb-5">
-        <div class="d-flex flex-row justify-content-end">
-            <a class="btn btn-outline-dark m-1" href="">connexion</a>
-            <a class="btn btn-outline-dark m-1" href="">Inscription</a>
-            
-        </div>
-        <h1 class="ml-5" style="font-family: 'Roboto Mono', monospace">A WORLD OF FACES</h1>
-        <h3  style="font-family: 'Indie Flower',cursive;margin-left: 15em">plus qu'un voyage . . .</h3>
-    </header>
 
-   <nav class="navbar d-flex flex-row justify-content-around mb-5 border rounded" style="border-color:#DCDCDC">
-        <a class="btn btn-outline-secondary" href="">ACCUEIL</a>
-        <a class="btn btn-outline-secondary" href="">QUI SUIS JE</a>
-        <a class="btn btn-outline-secondary" href="">STORE</a>
-    </nav>
+    <body class="container d-flex flex-column justify-content-center">
+      
     
-    <div class="container d-flex justify-content-center flex-column">  <!--2 Photos par ligne -->
+  
+    <?php include "../part-of-site/nav.php" ?>
+    
+    <div class="d-flex justify-content-center flex-column">  <!--2 Photos par ligne -->
         
         <div class="row">
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6 ">
                 <div class="card-body"> Titre de la photo </div>
                 <img class="card-img-bottom" src="../src/image2.png" alt="Card image cap">
                     <div class="row">
@@ -46,7 +17,7 @@
                     </div>
             </div>
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6 ">
                 <div class="card-body"> Some more card content </div>
                 <img class="card-img-bottom" src="../src/image3.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
@@ -57,7 +28,7 @@
 
         <div class="row">
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Titre de la photo </div>
                 <img class="card-img-bottom" src="../src/image4.png" alt="Card image cap">
                     <div class="row">
@@ -65,7 +36,7 @@
                     </div>
             </div>
 
-            <div class="card col-sm-12 col-md-5 m-1 ">
+            <div class="card col-sm-12 col-md-6 ">
                 <div class="card-body"> Some more card content </div>
                 <img class="card-img-bottom" src="../src/image5.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
@@ -74,7 +45,7 @@
 
         <div class="row">
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Titre de la photo </div>
                 <img class="card-img-bottom" src="../src/image6.png" alt="Card image cap">
                     <div class="row">
@@ -82,7 +53,7 @@
                     </div>
             </div>
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Some more card content </div>
                 <img class="card-img-bottom" src="../src/image7.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
@@ -91,7 +62,7 @@
 
         <div class="row">
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Titre de la photo </div>
                 <img class="card-img-bottom" src="../src/image8.png" alt="Card image cap">
                     <div class="row">
@@ -99,7 +70,7 @@
                     </div>
             </div>
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Some more card content </div>
                 <img class="card-img-bottom" src="../src/image9.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
@@ -108,7 +79,7 @@
 
         <div class="row">
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Titre de la photo </div>
                 <img class="card-img-bottom" src="../src/image10.png" alt="Card image cap">
                     <div class="row">
@@ -116,7 +87,7 @@
                     </div>
             </div>
 
-            <div class="card col-sm-12 col-md-5 m-1">
+            <div class="card col-sm-12 col-md-6">
                 <div class="card-body"> Some more card content </div>
                 <img class="card-img-bottom" src="../src/image11.png" alt="Card image cap">
                 <a class="btn btn-secondary btn-sm m-2 w-25" href="#!">Acheter</a>
